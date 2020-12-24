d5b9794f81efa899aa9cd4feb4224ae14083f497
na7esan <na7esan@gmail.com>
2020-12-24 06:51:46 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+        <h1>ログイン時は自分の質問一覧を表示する</h1>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', '今すぐ新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
